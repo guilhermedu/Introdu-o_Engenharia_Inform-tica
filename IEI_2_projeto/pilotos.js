43003b452ef30e1fda35848e36627c1be917bbdd
GuilhermeDuarte <duarte.g@ua.pt>
2024-03-10 18:08:55 +0000

    projetosIEI

---
+++ IEI_2_projeto/pilotos.js
@@ -0,0 +1,105 @@
+function appear(x) {
+
+    switch (x) {
+        case 1:
+            document.getElementById("racer1").src = "1.png";
+            document.getElementById("racer2").src = "2.png";
+            var oDiv = document.getElementById("nome1");
+            oDiv.innerHTML = "Miguel Oliveira";
+            var detalhes = document.getElementById("detalhes1");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Red Bull KTM Factory Racing <br> Mota : KTM <br> Data de Nascimento : 1995-01-04 <br> Local de Nascimento : Pragal <br> Altura : 170 cm | Peso : 64 kg  <br> " 
+            var oDiv = document.getElementById("nome2");
+            oDiv.innerHTML = "Brad Binder";
+            var detalhes = document.getElementById("detalhes2");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Red Bull KTM Factory Racing <br> Mota : KTM <br> Data de Nascimento : 1995-08-11  <br> Local de Nascimento : Potchefstroom <br> Altura : 170 cm | Peso : 63 kg  <br> " 
+            break;
+        case 2:
+            document.getElementById("racer1").src = "9.png";
+            document.getElementById("racer2").src = "10.png";
+            var oDiv = document.getElementById("nome1");
+            oDiv.innerHTML = "Marc Marquez";
+            var detalhes = document.getElementById("detalhes1");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Repsol Honda Team <br> Mota : Honda <br> Data de Nascimento : 1993-02-17 <br> Local de Nascimento : Cervera  <br> Altura : 169 cm | Peso : 65 kg  <br> " 
+            var oDiv = document.getElementById("nome2");
+            oDiv.innerHTML = "Pol Espargaro";
+            var detalhes = document.getElementById("detalhes2");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Repsol Honda Team <br> Mota : Honda <br> Data de Nascimento : 1991-06-10  <br> Local de Nascimento : Granollers  <br> Altura : 171 cm | Peso : 63 kg  <br> " 
+            break;
+        case 3:
+            document.getElementById("racer1").src = "3.png"
+            document.getElementById("racer2").src = "4.png";
+            var oDiv = document.getElementById("nome1");
+            oDiv.innerHTML = "Joan Mir";
+            var detalhes = document.getElementById("detalhes1");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Team SUZUKI ECSTAR <br> Mota : Suzuki <br> Data de Nascimento : 1997-09-01  <br> Local de Nascimento : Palma de Mallorca <br> Altura : 181 cm | Peso : 79 kg  <br> " 
+            var oDiv = document.getElementById("nome2");
+            oDiv.innerHTML = "Alex Rins";
+            var detalhes = document.getElementById("detalhes2");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Team SUZUKI ECSTAR <br> Mota : Suzuki <br> Data de Nascimento : 1995-12-08  <br> Local de Nascimento : Barcelona   <br> Altura : 176 cm | Peso : 72 kg  <br> " 
+            break;
+        case 4:
+            document.getElementById("racer1").src = "5.png";
+            document.getElementById("racer2").src = "6.png";
+            var oDiv = document.getElementById("nome1");
+            oDiv.innerHTML = "Jack Miller";
+            var detalhes = document.getElementById("detalhes1");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Ducati Lenovo Team <br> Mota : Ducati  <br> Data de Nascimento : 1995-01-18   <br> Local de Nascimento : Townsville  <br> Altura : 173 cm | Peso : 64 kg  <br> " 
+            var oDiv = document.getElementById("nome2");
+            oDiv.innerHTML = "Francesco Bagnaia";
+            var detalhes = document.getElementById("detalhes2");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Ducati Lenovo Team <br> Mota : Ducati <br> Data de Nascimento : 1997-01-14  <br> Local de Nascimento : Torino   <br> Altura : 176 cm | Peso : 67 kg  <br> "
+            break;
+        case 5:
+            document.getElementById("racer1").src = "11.png";
+            document.getElementById("racer2").src = "12.png";
+            var oDiv = document.getElementById("nome1");
+            oDiv.innerHTML = "Alex Marquez";
+            var detalhes = document.getElementById("detalhes1");
+            detalhes.innerHTML= "INFORMAÇÕES: <br>  LCR Honda Castrol <br> Mota : Honda  <br> Data de Nascimento : 1996-04-23   <br> Local de Nascimento : Cervera  <br> Altura : 179 cm | Peso : 65 kg  <br> " 
+            var oDiv = document.getElementById("nome2");
+            oDiv.innerHTML = "Takaaki Nakagami";
+            var detalhes = document.getElementById("detalhes2");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> LCR Honda Castrol <br> Mota : Honda <br> Data de Nascimento : 1992-02-09   <br> Local de Nascimento : Chiba    <br> Altura : 175 cm | Peso : 70 kg  <br> "
+           
+            break;
+        case 6:
+            document.getElementById("racer1").src = "13.png";
+            document.getElementById("racer2").src = "14.png";
+            var oDiv = document.getElementById("nome1");
+            oDiv.innerHTML = "Johann Zarco";
+            var detalhes = document.getElementById("detalhes1");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Pramac Racing  <br> Mota : Ducati  <br> Data de Nascimento : 1990-07-16   <br> Local de Nascimento : Cannes   <br> Altura : 171 cm | Peso : 67 kg  <br> " 
+            var oDiv = document.getElementById("nome2");
+            oDiv.innerHTML = "Jorge Martin";
+            var detalhes = document.getElementById("detalhes2");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Pramac Racing <br> Mota : Ducati <br> Data de Nascimento : 1998-01-29   <br> Local de Nascimento : Madrid    <br> Altura : 168 cm | Peso : 62 kg  <br> "
+            break;
+        case 7:
+            document.getElementById("racer1").src = "7.png";
+            document.getElementById("racer2").src = "8.png";
+            var oDiv = document.getElementById("nome1");
+            oDiv.innerHTML = "Raul Fernandez";
+            var detalhes = document.getElementById("detalhes1");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Tech3 KTM Factory Racing   <br> Mota : KTM  <br> Data de Nascimento : 2000-10-23    <br> Local de Nascimento : Madrid   <br> Altura : 177 cm | Peso : 63 kg  <br> " 
+            var oDiv = document.getElementById("nome2");
+            oDiv.innerHTML = "Remi Gardner";
+            var detalhes = document.getElementById("detalhes2");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Tech3 KTM Factory Racing <br> Mota : KTM <br> Data de Nascimento : 1998-02-24    <br> Local de Nascimento : Sydney    <br> Altura : 178 cm | Peso : 72 kg  <br> "
+            break;
+        case 8:
+
+            document.getElementById("racer1").src = "19.png";
+            document.getElementById("racer2").src = "20.png";
+            var oDiv = document.getElementById("nome1");
+            oDiv.innerHTML = "Fabio Quartararo";
+            var detalhes = document.getElementById("detalhes1");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Monster Energy Yamaha MotoGP™  <br> Mota : Yamaha  <br> Data de Nascimento : 1999-04-20   <br> Local de Nascimento : Nice  <br> Altura : 177 cm | Peso : 66 kg  <br> " 
+            var oDiv = document.getElementById("nome2");
+            oDiv.innerHTML = "Franco Morbidelli";
+            var detalhes = document.getElementById("detalhes2");
+            detalhes.innerHTML= "INFORMAÇÕES: <br> Monster Energy Yamaha MotoGP™  <br> Mota : Yamaha  <br> Data de Nascimento : 1994-12-04    <br> Local de Nascimento : Rome  <br> Altura : 176 cm | Peso : 64 kg  <br> " 
+            break;
+        
+
+    }
+};
